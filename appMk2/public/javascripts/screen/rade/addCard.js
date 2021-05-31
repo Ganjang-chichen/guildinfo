b254bdd092cb59b3e54bbe42c347e62b3091d2a2
Ganjang-chichen <59672593+Ganjang-chichen@users.noreply.github.com>
2021-05-31 19:08:50 +0900

    card sorting

--- appMk2/public/javascripts/screen/rade/addCard.js
+++ appMk2/public/javascripts/screen/rade/addCard.js
@@ -1,8 +1,54 @@
 
+function clsUpperSet(c) {
+    let cls = c;
+    if(c === '파이터' || c === '크루세이더'){
+        cls = '히어로';
+    }else if(c === '페이지' || c === '나이트'){
+        cls = '팔라딘';
+    }else if(c === '스피어맨' || c === '버서커'){
+        cls = '다크나이트';
+    }else if(c === '위자드(불,독)' || c === '메이지(불,독)'){
+        cls = '아크메이지(불,독)';
+    }else if(c === '위자드(썬,콜)' || c === '메이지(썬,콜)'){
+        cls = '아크메이지(썬,콜)';
+    }else if(c === '클레릭' || c === '프리스트'){
+        cls = '비숍';
+    }else if(c === '헌터' || c === '레인저'){
+        cls = '보우마스터';
+    }else if(c === '사수' || c === '저격수'){
+        cls = '신궁';
+    }else if(c === '위자드(썬,콜)' || c === '메이지(썬,콜)'){
+        cls = '아크메이지(썬,콜)';
+    }else if(c === '어쌔신' || c === '허밋'){
+        cls = '나이트로드';
+    }else if(c === '시프' || c === '시프마스터'){
+        cls = '섀도어';
+    }else if(c === '인파이터' || c === '버커니어'){
+        cls = '바이퍼';
+    }else if(c === '건슬링거' || c === '발키리'){
+        cls = '캡틴';
+    }else if(c === '캐논슈터' || c === '캐논블래스터'){
+        cls = '캐논마스터';
+    }else if(c === '세미듀어러' || c === '듀어러' || c === '듀얼마스터' || c === '슬래셔'){
+        cls = '듀얼블레이더';
+    }else if(c === 'X' || c === "" || c === null){
+        cls = '초보자';
+    }
+
+    return cls;
+}
+
 function cardTextSetting(isMaster, id, lv, cls, doj_high, doj_cur, img) {
+    if(doj_high === null || doj_high === ""){
+        doj_high = "00층";
+    }
+    if(doj_cur === null || doj_cur === ""){
+        doj_cur = "00층";
+    }
+
     let card = `
         <div class="char_card" draggable="${isMaster}">
-            <img src="images/class_char_img/${cls}.png" alt="" class="card_class_img">
+            <img src="images/class_char_img/${clsUpperSet(cls)}.png" alt="" class="card_class_img">
             <div class="gradation"></div>
             <div class="card_info">
                 <div>
@@ -10,7 +56,7 @@ function cardTextSetting(isMaster, id, lv, cls, doj_high, doj_cur, img) {
                     <div class="card_LV">LV: ${lv}</div>
                 </div>
                 <div>
-                    <div class="card_class">직업: ${cls}</div>
+                    <div class="card_class">직업: ${clsUpperSet(cls)}</div>
                     
                 </div>
                 <div>
@@ -113,3 +159,41 @@ function init(){
 }
 
 init();
+
+function sortingCard() {
+
+    let selectOpt = document.querySelector(".rade_options_sorting");
+    let opt = selectOpt.options[selectOpt.selectedIndex].value.toString();
+
+    console.log("sorting~ " + opt);
+
+    let default_list = default_party.querySelectorAll(".char_card");
+    let list_info = new Array();
+
+    for(let i = 0; i < default_list.length; i++) {
+        let arr = {
+            idx : i,
+            name : default_list[i].querySelector(".card_name").textContent.replace("ID: ", ""),
+            className : default_list[i].querySelector(".card_class").textContent.replace("직업: ", ""),
+            best : default_list[i].querySelector(".card_best_dojang").textContent.replace("무릉 최고: ", "").replace("층", ""),
+            latest : default_list[i].querySelector(".card_latest_dojang").textContent.replace("최근: ", "").replace("층", "")
+        }
+        list_info.push(arr);
+    }
+
+    list_info.sort((a, b) => {
+        if (a[opt] < b[opt]) {
+            return 1;
+        }
+        if (a[opt] > b[opt]) {
+            return -1;
+        }
+        return 0;
+    });
+
+    default_list.innerHTML = '';
+    for(let i = 0; i < list_info.length; i++) {
+        default_party.appendChild(default_list[list_info[i].idx]);
+    }
+
+}
